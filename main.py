0dc154ce039b022c7c0fc3e7ce30d724325b5ed5
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-03-25 19:38:38 -0300

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from llm-learning!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    result =client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+
+    print(result.text)
 
 if __name__ == "__main__":
     main()
